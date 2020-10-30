1b69025d1304d104c48d73056ce6fb5e52603eaa
jerry joy <jerryjoy99@gmail.com>
2020-10-30 16:48:33 +0530

    deploy to heroku

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const chatbotController = require("../controllers/chatbotController");
 const homePageCOntroller = require("../controllers/homepageController");
 const router = express.Router();
 
 const initWebRoutes = (app) => {
   router.get("/", homePageCOntroller.getHomePage);
+  router.get("/webhook", chatbotController.getWebHooks);
+  router.post("/webhook", chatbotController.postWebhooks);
   return app.use("/", router);
 };
 
